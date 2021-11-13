6815b8b9fee3c5e827a4df62ea9e69182c3c093f
or lantsman <or474894@gmail.com>
2021-11-13 17:53:41 +0200

    hello

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ initAll();
 function initAll() {
     initButtons();
     initGame();
+    if (screen.height > 700) {
+        g_state.board.style.minHeight = "500px";
+    }
     window.addEventListener('resize', function (event) {
         g_state.control_panel.style.top = (g_state.board.offsetHeight + g_state.board.offsetTop + 10) + "px";
         if (!g_state.game_loop) {
